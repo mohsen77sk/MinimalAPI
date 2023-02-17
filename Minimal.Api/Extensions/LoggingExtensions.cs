8ef92c8c81befebef4462b57ebfa4451932d57b2
Mohsen-MSK <mohsen77sk@gmail.com>
2023-02-17 20:40:28 +0330

    add logging extensions file

---
+++ Minimal.Api/Extensions/LoggingExtensions.cs
@@ -0,0 +1,22 @@
+namespace Minimal.Api.Extensions;
+
+public static class LoggingExtensions
+{
+    public static WebApplicationBuilder AddApplicationLogging(this WebApplicationBuilder builder)
+    {
+        if (builder == null)
+        {
+            throw new ArgumentNullException(nameof(builder));
+        }
+
+        builder.Logging.ClearProviders();
+        if (builder.Environment.IsDevelopment())
+        {
+            builder.Logging.AddConsole();
+            builder.Logging.AddDebug();
+        }
+        builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+
+        return builder;
+    }
+}
--- Minimal.Api/Program.cs
+++ Minimal.Api/Program.cs
@@ -1,15 +1,7 @@
 using Minimal.Api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-// Configure logging
-builder.Logging.ClearProviders();
-if (builder.Environment.IsDevelopment())
-{
-    builder.Logging.AddConsole();
-    builder.Logging.AddDebug();
-}
-builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
-// Configure services
+builder.AddApplicationLogging();
 builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
